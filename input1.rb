cf86340618f35fda1d93006116d15876de8b49d2
Mutiajaya <mutiiaraannuur@gmail.com>
2022-07-08 15:35:00 +0700

    Menambahkan file1.rb

---
+++ input1.rb
@@ -0,0 +1,10 @@
+#input
+
+print "Harga outflitnya berapa kakak ??"
+outflit = gets.chomp
+print "Belinya dimana kakak ??"
+beli = gets.chomp
+
+print "owalah harga outflit kakaknya #{outflit}, belinya pasti di #{beli} ya kak!!"
+
+
