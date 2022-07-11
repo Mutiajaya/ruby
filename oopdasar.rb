05c0b220cdb17a78c0efb0e22ec98cefd5e17b60
Mutiajaya <mutiiaraannuur@gmail.com>
2022-07-11 14:16:19 +0700

    Menambahkan file oopdasar.rb

---
+++ oopdasar.rb
@@ -0,0 +1,14 @@
+#OOP - Object Oriented Programming
+#DRY
+
+class Orang
+    
+    def ambilUmur
+        puts "Umurnya :"
+    end
+    
+end
+
+#bikin objek dari kelas
+orang = Orang.new
+orang.ambilUmur()
