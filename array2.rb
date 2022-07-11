24526041d50cd50faa01383e9fd366ecd44f9cb3
Mutiajaya <mutiiaraannuur@gmail.com>
2022-07-11 11:06:07 +0700

    Menambahkan syntax untuk menghapus elemen

--- array2.rb
+++ array2.rb
@@ -6,4 +6,10 @@ bahasa.push("Ruby") #diakhir
 bahasa.unshift("C++") #diawal
 bahasa.insert(2, "Swift") #ditengah
 
+bahasa.pop #untukmenghapuselementerakhir
+puts bahasa.pop
+
+puts "ngeluarin nilai #{bahasa.shift}" #untukmenghapuselemenawal
+puts "ngeluarin nilai #{bahasa[2]}"
+
 puts bahasa
