eeda5f926fbeedc33642135b31fa25d462ac5de9
Mutiajaya <mutiiaraannuur@gmail.com>
2022-07-11 12:10:25 +0700

    Menambahkan syntax each

--- loops.rb
+++ loops.rb
@@ -7,6 +7,8 @@ while i < 5
     i += 1
 end
 
+puts "------------------"
+
 x=3
 
 until x <= 0
@@ -14,15 +16,34 @@ until x <= 0
     x -= 1
 end
 
+puts "------------------"
+
 for angka in (0..5)
 
     puts "Nilai angka : #{angka}"
 
 end
 
+puts "------------------"
+
 #contohforin
 
 bahasa = ["html", "css", "js"]
 for b in bahasa
     puts "nilai bahasa : #{b}"
+end
+
+puts "------------------"
+
+bahasa.each do |b|
+    puts "nilai bahasa2 : #{b}"
+end
+
+puts "------------------"
+bahasa.each {|b| puts "disini juga : #{b}"}
+
+puts "------------------"
+
+(0..3).each do |a|
+    puts "Nilai y #{a}"
 end
