d08746d000e9e956c8f79b615b04e5011d22d741
Mutiajaya <mutiiaraannuur@gmail.com>
2022-07-11 10:17:43 +0700

    Menambahkan file array1.rb

---
+++ array1.rb
@@ -0,0 +1,12 @@
+#array
+
+angka = [1, 2, 3]
+bahasa = ["HTML", "PHP", "Java", "CSS", "JS"]
+
+puts "Bahasa yang Pertama : " + bahasa.first
+puts "Bahasa yang Terakhir : " + bahasa.last
+
+#drop
+puts "2 contoh Bahasa : #{bahasa.take(2)} " 
+puts "Jumlah Bahasa : #{bahasa.count} " 
+puts "Length Bahasa : #{bahasa.length}"
